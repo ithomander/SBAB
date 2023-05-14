b2f674f8ce83635845a6bfe74687dbe4f2c78679
ithomander <isabelthomander@hotmail.com>
2023-05-14 14:18:09 +0200

    Removed strict mode, rendering once

--- client/src/components/BusStops/BusStops.js
+++ client/src/components/BusStops/BusStops.js
@@ -1,19 +1,7 @@
 import React from 'react';
 
-function sortToColumns(list, limit) {
-    const columns = list.reduce((acc, element) => {
-        if (acc.length === 0 || acc[acc.length - 1].length === limit) {
-          acc.push([element]);
-        } else {
-          acc[acc.length - 1].push(element);
-        }
-        return acc;
-      }, []);
-    return columns
-}
-
 export default function BusStops({stopsList}) {    
-    const columns = sortToColumns(stopsList, 60)
+    const columns = sortToColumns(stopsList, 70)
 
     return (
         <div className='container-fluid flex mx-0'>
@@ -30,4 +18,16 @@ export default function BusStops({stopsList}) {
             </div>
         </div>
     )
+}
+
+function sortToColumns(list, limit) {
+    const columns = list.reduce((acc, element) => {
+        if (acc.length === 0 || acc[acc.length - 1].length === limit) {
+          acc.push([element]);
+        } else {
+          acc[acc.length - 1].push(element);
+        }
+        return acc;
+      }, []);
+    return columns
 }
