8a5f4af60e5862d2aa4e7d61b32e2008b8378379
ithomander <isabelthomander@hotmail.com>
2023-05-14 11:11:39 +0200

    Refactored server index

--- server/index.js
+++ server/index.js
@@ -4,60 +4,58 @@ const keys = require('./config/keys');
 
 const app = express(); 
 
-app.get("/api", async (req, res) => {
-    try {
-        const [busLines, stopNames] = await Promise.all([getBusLines(), getStopNames()]);
-        updateBusStopWithNames(busLines, stopNames);
-        res.send(busLines);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(error.response.data.message);
-    }
+app.get("/api", (req, res) => {
+    Promise.all([getBusLines(), getStopNames()])
+        .then(([busLines, stopNames]) => {
+            updateBusStopWithNames(busLines, stopNames);
+            res.send(busLines);
+        }).catch(error => {
+            console.log(error);
+            res.status(500).send("Could not retrieve bus lines and stops");
+        })
 });
 
-async function getBusLines() {
-    const response = await axios({
+function getBusLines() {
+    return axios({
         method: 'get',
         url: `https://api.sl.se/api2/LineData.json?model=jour&key=${keys.trafikLabKey}&DefaultTransportModeCode=BUS`,
         headers: {'Accept-Encoding': 'gzip, deflate'}
+    }).then(response => {
+        const result = response.data.ResponseData.Result
+        
+        // Create an array with objects of bus line and its stop numbers
+        const stopsPerLine = result.reduce((acc, stop) => {
+            if (acc.some(element => element.line === stop.LineNumber)) {
+                const lineIndex = acc.findIndex(element => element.line === stop.LineNumber)
+                acc[lineIndex].busStops.push(stop.JourneyPatternPointNumber)
+            } else {
+                acc.push({line: stop.LineNumber, busStops: [stop.JourneyPatternPointNumber]})
+            }
+            return acc;
+        }, []);
+
+        // Select top ten bus lines
+        const topBusLines = stopsPerLine
+            .sort((a, b) => b.busStops.length - a.busStops.length)
+            .slice(0,10);
+            
+        return topBusLines
     })
-
-    const result = response.data.ResponseData.Result
-        
-    // Create an array with objects of bus line and its stop numbers
-    const stopsPerLine = result.reduce((acc, stop) => {
-        if (acc.some(element => element.line === stop.LineNumber)) {
-            const lineIndex = acc.findIndex(element => element.line === stop.LineNumber)
-            acc[lineIndex].busStops.push(stop.JourneyPatternPointNumber)
-        } else {
-            acc.push({line: stop.LineNumber, busStops: [stop.JourneyPatternPointNumber]})
-        }
-        return acc;
-    }, []);
-
-    // Select top ten bus lines
-    const topBusLines = stopsPerLine
-        .sort((a, b) => b.busStops.length - a.busStops.length)
-        .slice(0,10);
-        
-    return topBusLines
-    
 }
 
-async function getStopNames() {
-    const response = await axios({
+function getStopNames() {
+    return axios({
         method: 'get',
         url: `https://api.sl.se/api2/LineData.json?model=stop&key=${keys.trafikLabKey}`,
         headers: {'Accept-Encoding': 'gzip, deflate'}
+    }).then(response => {
+        const result = response.data.ResponseData.Result
+    
+        // Create a dictionary with bus stop number and name
+        const stopNames = Object.fromEntries(result.map(stop => [stop.StopPointNumber, stop.StopPointName]))
+        
+        return stopNames
     })
-
-    const result = response.data.ResponseData.Result
-    
-    // Create a dictionary with bus stop number and name
-    const stopNames = Object.fromEntries(result.map(stop => [stop.StopPointNumber, stop.StopPointName]))
-    
-    return stopNames
-
 }
 
 function updateBusStopWithNames(busLines, stopNames) {
