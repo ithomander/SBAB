929aa0c8d8f63a7e7a3413a98f530d19afbea5bf
ithomander <isabelthomander@hotmail.com>
2023-05-14 17:48:58 +0200

    Added manifest again

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1">
                     <img src="/bus_logo.png" alt="Bus logo" width="60" height="60"/>
                     <span>Top Ten Bus</span>
                 </span>
